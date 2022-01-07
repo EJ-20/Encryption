d413f081048dbacf5277ce38457a65d5991bce0e
Erfan Jafar <67335103+EJ-20@users.noreply.github.com>
2022-01-07 19:37:27 -0330

    first

---
+++ src/com/company/Encrypt.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Encrypt {
+
+    public void encrypt() {
+        Scanner message = new Scanner(System.in);
+        String text = message.toString();
+        System.out.println(text);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Scanner message = new Scanner(System.in);
+        Random rand = new Random();
+
+    // getting user input
+        System.out.println("Enter your message: ");
+        String input = message.nextLine();
+
+
+    // creating key for encryption
+        int size = input.length();
+        String key =
+
+    }
+}
